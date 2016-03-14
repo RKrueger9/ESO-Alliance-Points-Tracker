a551b99dff4bc7f5864feda0d1a4a9e14027bc12
RKrueger <student@rkrueger.local>
2016-03-14 12:30:48 -0500

    Set up Storyboard

--- ESO Alliance Points Tracker/ViewController.swift
+++ ESO Alliance Points Tracker/ViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
     }
-
-
+    
 }
 
